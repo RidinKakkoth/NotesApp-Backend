85811ccc74e5c3f55545a9444fc66c3d8746dd3f
Ridin Kakkoth <k.ridin@ymail.com>
2024-08-25 19:31:54 +0530

    api update

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const cors=require('cors')
 require('dotenv').config();
 
 const app=express()
-app.use(cors())
+
+const corsOptions = {
+    origin: 'https://notesapp-frontend-3x3v.onrender.com', 
+    methods: 'GET,POST,PUT,DELETE', 
+    allowedHeaders: 'Content-Type,Authorization' 
+};
+
+app.use(cors(corsOptions));
 app.use(express.json())
 
 
